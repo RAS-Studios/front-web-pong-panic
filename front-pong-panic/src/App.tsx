56c91dcdfb6e0fbe4e50b5fe1e6f1af592764b90
Sonny Naidja <133282829+NASonny@users.noreply.github.com>
2026-07-14 15:28:32 +0200

    [~] main: Comment out GameplayVideo import
    Commented out the import statement for GameplayVideo.

--- front-pong-panic/src/App.tsx
+++ front-pong-panic/src/App.tsx
@@ -1,6 +1,6 @@
 import { Introduction } from '@/components/Introduction';
 import { Features } from '@/components/Features';
-import { GameplayVideo } from '@/components/GameplayVideo';
+ {/* import { GameplayVideo } from '@/components/GameplayVideo';*/}
 import { Gallery } from '@/components/Gallery';
 import { Stats } from '@/components/Stats';
 import { Team } from '@/components/Team';
